905ddbfb28bb48de7988eeb1ead770d30cf7bd27
RkEclair <ryosukadnak@gmail.com>
2017-07-03 18:54:35 +0900

    Add ending

--- main.cpp
+++ main.cpp
@@ -48,81 +48,94 @@ int main() {
 
 	glViewport(0, 0, width, height);
 
+	bool quit = false;
+	do {
 	// TileMap's format:
 	// 5x5, 0.Wall, 1.Space, 2.Start, 3.Goal, 4.Locked Door
 	// 5~8.Medium Case(Left, Down, Right, Up), 9~12.Large Case(Left, Down, Right, Up)
 	// 13~16.Medium Key Case(Left, Down, Right, Up), 17~20.Large Key Case(Left, Down, Right, Up)
-	StageData stage1({
-		0, 0, 0, 0, 0,
-		0, 0, 0, 0, 0,
-		2, 1, 1, 1, 3,
-		0, 0, 0, 0, 0,
-		0, 0, 0, 0, 0,
-	});
-	while (stage1.mainLoop(window)) {;}
+		StageData stage1({
+			0, 0, 0, 0, 0,
+			0, 0, 0, 0, 0,
+			2, 1, 1, 1, 3,
+			0, 0, 0, 0, 0,
+			0, 0, 0, 0, 0,
+		});
+		while (stage1.mainLoop(window)) {;}
 
-	StageData stage2({
-		2, 0, 0, 0, 0,
-		1, 1, 0, 0, 0,
-		0, 1, 1, 0, 0,
-		0, 0, 1, 1, 0,
-		0, 0, 0, 1, 3,
-	});
-	while (stage2.mainLoop(window)) {;}
+		StageData stage2({
+			2, 0, 0, 0, 0,
+			1, 1, 0, 0, 0,
+			0, 1, 1, 0, 0,
+			0, 0, 1, 1, 0,
+			0, 0, 0, 1, 3,
+		});
+		while (stage2.mainLoop(window)) {;}
 
-	StageData stage3({
-		0, 0, 0, 0, 0,
-		1, 1,17, 0, 0,
-		2, 1, 1, 4, 3,
-		1,13, 1, 0, 0,
-		0, 0, 0, 0, 0,
-	});
-	while (stage3.mainLoop(window)) {;}
+		StageData stage3({
+			0, 0, 0, 0, 0,
+			1, 1,17, 0, 0,
+			2, 1, 1, 4, 3,
+			1,13, 1, 0, 0,
+			0, 0, 0, 0, 0,
+		});
+		while (stage3.mainLoop(window)) {;}
 
-	StageData stage4({
-		0, 0, 0, 0, 0,
-		0, 2, 1, 9, 0,
-		0, 1,20, 1, 0,
-		0,16, 1, 1, 4,
-		0, 0, 0, 0, 3,
-	});
-	while (stage4.mainLoop(window)) {;}
+		StageData stage4({
+			0, 0, 0, 0, 0,
+			0, 2, 1, 9, 0,
+			0, 1,20, 1, 0,
+			0,16, 1, 1, 4,
+			0, 0, 0, 0, 3,
+		});
+		while (stage4.mainLoop(window)) {;}
 
-	StageData stage5({
-		0, 0, 0, 0, 0,
-		1,10, 1, 0, 0,
-		15, 2,18, 4, 3,
-		1,12, 1, 0, 0,
-		0, 0, 0, 0, 0,
-	});
-	while (stage5.mainLoop(window)) {;}
+		StageData stage5({
+			0, 0, 0, 0, 0,
+			1,10, 1, 0, 0,
+			15, 2,18, 4, 3,
+			1,12, 1, 0, 0,
+			0, 0, 0, 0, 0,
+		});
+		while (stage5.mainLoop(window)) {;}
 
-	StageData stage6({
-		1, 1,19, 1, 0,
-		1, 8,16, 2, 0,
-		0, 4, 0, 0, 0,
-		0, 1, 0, 0, 0,
-		0, 3, 0, 0, 0,
-	});
-	while (stage6.mainLoop(window)) {;}
+		StageData stage6({
+			1, 1,19, 1, 0,
+			1, 8,16, 2, 0,
+			0, 4, 0, 0, 0,
+			0, 1, 0, 0, 0,
+			0, 3, 0, 0, 0,
+		});
+		while (stage6.mainLoop(window)) {;}
 
-	StageData stage7({
-		0, 0, 0, 0, 0,
-		1, 1, 1,10, 1,
-		1,20, 9, 2,16,
-		0, 4, 0, 0, 0,
-		0, 3, 0, 0, 0,
-	});
-	while (stage7.mainLoop(window)) {;}
+		StageData stage7({
+			0, 0, 0, 0, 0,
+			1, 1, 1,10, 1,
+			1,20, 9, 2,16,
+			0, 4, 0, 0, 0,
+			0, 3, 0, 0, 0,
+		});
+		while (stage7.mainLoop(window)) {;}
 
-	StageData stage8({
-		0, 2, 0, 1,10,
-		0, 1, 1, 1, 1,
-		0,15, 1, 1,20,
-		0, 1,12, 1, 4,
-		0, 0, 0, 0, 3,
-	});
-	while (stage8.mainLoop(window)) {;}
+		StageData stage8({
+			0, 0, 2, 1, 1,
+			1, 1,11, 1, 1,
+		 20,13, 0,11, 8,
+			1, 5, 0, 0, 0,
+			1, 1, 1, 4, 3,
+		});
+		while (stage8.mainLoop(window)) {;}
+
+		// Ending
+		StageData ending({
+			2, 1, 1, 1, 1,
+			0, 0, 0, 0, 1,
+			1, 1, 1, 1, 1,
+			1, 0, 0, 0, 0,
+			1, 1, 1, 1, 3,
+		});
+		quit = !(ending.mainLoop(window));
+	} while(!quit);
 
 	std::cout<< "Exiting..."<<std::endl;
 
